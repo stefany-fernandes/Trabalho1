774f1de3b3395af215a295b7ce732112015a87d1
Stefany <stefanyfer.sf@outlook.com>
2016-09-27 10:07:48 -0300

    teste1

--- Validator/testes/bank/BankTest.php
+++ Validator/testes/bank/BankTest.php
@@ -13,10 +13,108 @@ use Validator\Bank\Bank;
 
 class BankTest extends \PHPUnit_Framework_TestCase
 {
-    public function testConstructor(){
-        $bankObj = new Bank('0635', '0020173-1');
-        $this->assertEquals($bankObj->toInteger(), 63500201731);
-        $this->assertEquals($bankObj->toFormatted(), '0635 0020173-1');
+    /**
+     * @param string $agencia
+     * @param string $conta
+     * @dataProvider providerTest
+     *
+     */
+    public function testConstructorValidAgencia(string $agencia, string $conta){
+        $bankObj1 = new Bank($agencia,$conta);
+        $this->assertEquals($bankObj1->toFormatted(), "$agencia $conta");
     }
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTest
+     *
+     */
+
+    public function testConstructorInvalidAgencia(string $agencia, string $conta) {
+        $bankObj2 = new Bank ($agencia, $conta);
+        $this->assertEquals($bankObj2->validateAg(), true);
+    }
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTest
+     */
+
+    public function testConstructorValidateAcc(string $agencia, string $conta) {
+        $bankObj3 = new Bank($agencia, $conta);
+        $this->assertEquals($bankObj3->validateC(), true);
+    }
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTestErro
+     */
+
+    public function testeContaInvalida (string $agencia, string $conta){
+        $bankObj4 = new Bank($agencia, $conta);
+        $this->assertEquals($bankObj4->validateC(), false);
+
+    }
+
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTestErro
+     */
+
+    public function testeAgenciaInvalida(string $agencia, string $conta){
+        $bankObj5 = new Bank ($agencia, $conta);
+        $this->assertEquals($bankObj5->validateAg(), false);
+    }
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTest
+     */
+
+    public function TesteFinalContaAgValidas(string $agencia, string $conta) {
+        $bankObj6 = new Bank ($agencia, $conta);
+        $this->assertEquals($bankObj6->validate(), true);
+    }
+
+    /**
+     * @param string $agencia
+     * @param string $conta
+     *
+     * @dataProvider providerTest
+     */
+    public function testToInteger(string $agencia, string $conta){
+        $bankObj7 = new Bank($agencia, $conta);
+        $this->assertEquals($bankObj7->toInteger(), 63500201731);
+    }
+
+    /**
+     * @return array
+     */
+
+
+    public function providerTestErro() {
+        return [
+            ['0633', '0020179-1'],
+
+        ];
+    }
+
+    public function providerTest() {
+        return [
+          ['0635', '0020173-1'],
+        ];
+    }
+
 
 }
